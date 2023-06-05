6495dded91ed5ec65a5dc33a01fb5fbdd7f0867b
jvschamne <schamnejv@hotmail.com>
2023-06-05 18:34:32 -0300

    tela de carrinho

--- screens/Carrinho.js
+++ screens/Carrinho.js
@@ -1,14 +1,39 @@
-import { StyleSheet, Text, View, Image, ScrollView } from 'react-native';
+import { StyleSheet, Text, View, Image, ScrollView, TouchableOpacity } from 'react-native';
 import BottomTabNav from '../components/BottomTabNav';
+import OptionCard from '../components/OptionCard';
 
 export default function Carrinho(props) {
+
+
+
+    const pedidoTemporario = [
+        ["X-Burguer", "R$22,90"], 
+        ["Pizza", "R$42,90"], 
+        ["Churros", "R$12,90"],
+        ["Sonho", "R$5,90"],
+        ["X-Burguer", "R$22,90"], 
+    ]
+
     return(
         <View style={styles.container}>
-            <Text>Carrinho</Text>
+            <Text style={styles.title}>Seu pedido</Text> 
+            <ScrollView contentContainerStyle={styles.scrollViewContent} style={{marginBottom: 300}}>
+                    {pedidoTemporario.length != 0 &&
+                        pedidoTemporario.map((opcao, i) => <OptionCard key={i} name={opcao[0]} price={opcao[1]}/>)
+                    }
+            </ScrollView>
+            
+            <View style={styles.pagamento}>
+                <Text style={{fontSize: 20, marginBottom: 30}}>Total a pagar: R$43,99</Text>
+                <TouchableOpacity style={styles.checkoutButton}>
+                    <Text style={styles.textButton}>Finalizar pedido</Text>
+                </TouchableOpacity>
+            </View>
             <BottomTabNav></BottomTabNav>
         </View>
     )
 }
+/*
 
 const styles = StyleSheet.create({
     container: {
@@ -17,4 +42,68 @@ const styles = StyleSheet.create({
         justifyContent: 'flex-start',
         backgroundColor: '#f9f1f7',
     },
-})
+    title: {
+        fontSize: 30,
+        fontWeight: 'bold',
+        marginTop: 50,
+        marginBottom: 50,
+    },
+    checkoutButton: {
+        backgroundColor: '#fcc40d',
+        padding: 20,
+        borderRadius: 25,
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    textButton: {
+        color: 'black',
+        fontWeight: 'bold',
+        fontSize: 20,
+    },
+    pagamento: {
+        position: 'absolute',
+        bottom: 150, // Ajuste conforme necessário
+    },
+    scrollViewContent: {
+        flexGrow: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+});*/
+
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        alignItems: 'center',
+        backgroundColor: '#f9f1f7',
+    },
+    title: {
+        fontSize: 30,
+        fontWeight: 'bold',
+        marginTop: 50,
+        marginBottom: 20,
+    },
+    checkoutButton: {
+        backgroundColor: '#fcc40d',
+        padding: 20,
+        borderRadius: 25,
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    textButton: {
+        color: 'black',
+        fontWeight: 'bold',
+        fontSize: 20,
+    },
+    pagamento: {
+        position: 'absolute',
+        bottom: 150, // Ajuste conforme necessário
+    },
+    scrollViewContent: {
+        flexGrow: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
+        marginBottom: 150,
+    },
+});
